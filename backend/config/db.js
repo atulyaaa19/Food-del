44d6b18633492184ec7ed2e4ec7ac84eb36bd92b
atulyaaa19 <atulyajayant.sahane2022@vitstudent.ac.in>
2025-06-19 17:29:53 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 export const  connectDB = async () =>{
-
-    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
+const url=process.env.MONGO_DB;
+    await mongoose.connect(url).then(()=>console.log("DB Connected"));
    
 }
 
